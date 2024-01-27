12367688c5979d06e7dac3a5708c2c2089826afd
omoleen <Oreoluwa123>
2024-01-27 11:26:03 +0100

    fixed index.js bug

--- registration/src/index.js
+++ registration/src/index.js
@@ -1,42 +1,11 @@
 // src/index.js
-import React, { useState } from 'react';
-import StepOne from './components/StepOne';
-// ...import other steps
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {RegistrationForm} from './components/RegistrationForm'
 
-const RegistrationForm = () => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const [formData, setFormData] = useState({
-    country: '',
-    username: '',
-    // ...other fields
-  });
-
-  // Function to update the formData state
-  const updateFormData = (newData) => {
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      ...newData
-    }));
-  };
-
-  // Function to proceed to the next step
-  const nextStep = () => {
-    setCurrentStep(prevCurrentStep => prevCurrentStep + 1);
-  };
-
-  // ...JSX to render the current step
-  return (
-    <div>
-      {currentStep === 1 && (
-        <StepOne
-          formData={formData}
-          updateFormData={updateFormData}
-          nextStep={nextStep}
-        />
-      )}
-      {/* ...other steps conditionally rendered */}
-    </div>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+        <RegistrationForm />
+    </React.StrictMode>
   );
-};
-
-export default RegistrationForm;
